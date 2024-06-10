826f158089df2fc62132410981336d19ce34497f
Uday Beswal <71140717+UdBe@users.noreply.github.com>
2024-06-10 11:23:29 +0530

    Time: 70 ms (12.45%) | Memory: 66 MB (10.92%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -5,13 +5,16 @@ public:
                vector<int> candidates, int target, int i) {
 
         // Base Case
-        if(i == candidates.size()){
-            if(target==0){
-                answer.push_back(combination);
-            } 
-            return;
+        if(target == 0){
+            answer.push_back(combination);
+            return; 
         }
 
+        if(target < 0 || i == candidates.size()){
+            return;
+        }
+        
+        //Recursive Calls
         solve(answer, combination, candidates, target, i + 1);
         if (candidates[i] <= target) {
             combination.push_back(candidates[i]);
